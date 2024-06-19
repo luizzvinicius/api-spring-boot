c7fa8424a2d2e29e3fb95fef53518651284cacd3
Luiz Vinicius <viniciusadr244@gmail.com>
2024-06-19 19:01:01 -0300

    feat: service tests

--- src/test/java/luiz/api/products/service/ProductServiceTest.java
+++ src/test/java/luiz/api/products/service/ProductServiceTest.java
@@ -3,57 +3,79 @@ package luiz.api.products.service;
 import luiz.api.products.dto.ProductDTO;
 import luiz.api.products.dto.mapper.ProductMapper;
 import luiz.api.products.enums.ProductStatus;
+import luiz.api.products.exceptions.RecordNotFoundExt;
 import luiz.api.products.model.Product;
 import luiz.api.products.repository.ProductRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
-@DataJpaTest
 @ActiveProfiles("test")
-// @ExtendWith(MockitoExtension.class) // substitui o openmocks e autoclosesable
+@ExtendWith(MockitoExtension.class) // substitui o openmocks e autoclosesable
 class ProductServiceTest {
-    private AutoCloseable mockito;
     @Mock
     private ProductRepository repository;
 
     @Mock
-    private ProductMapper map;
+    private ProductMapper mapper;
 
     @InjectMocks
     private ProductService service;
 
-    @BeforeEach
-    void setup() {
-        mockito = MockitoAnnotations.openMocks(this);
-    }
+    private Product product;
+    private ProductDTO productDTO;
 
-    @AfterEach
-    void tearDown() throws Exception {
-        mockito.close();
+    @BeforeEach
+    void init() {
+       productDTO = new ProductDTO(UUID.randomUUID(), "teste Service", 1690d);
+       product = new Product(productDTO.id(), "teste Service", 1690d, ProductStatus.ACTIVE);
     }
 
     @Test
-    @DisplayName("Service save product without problems")
-    void saveProduct() {
-        var productDTO = new ProductDTO(UUID.randomUUID(), "teste Service", 1690d);
-        var product = new Product(productDTO.id(), "teste Service", 1690d, ProductStatus.ACTIVE);
+    void productService_saveProduct_returnProductDTO() {
+        when(mapper.toEntity(productDTO)).thenReturn(product);
+        when(repository.save(any(Product.class))).thenReturn(product);
+        when(mapper.toDTO(product)).thenReturn(productDTO);
 
-        when(map.toEntity(productDTO)).thenReturn(product);
-        when(repository.save(product)).thenReturn(product);
-        when(map.toDTO(product)).thenReturn(productDTO);
+        var savedProductDTO = service.saveProduct(productDTO);
 
-        assertThat(productDTO.id()).isEqualTo(product.getId());
+        assertThat(savedProductDTO).isEqualTo(productDTO);
     }
+
+    @Test
+    void productService_getOneProduct_returnProduct() {
+        var id = UUID.randomUUID();
+
+        when(repository.findById(id)).thenReturn(Optional.of(product));
+        when(mapper.toDTO(product)).thenReturn(productDTO);
+
+        var serviceFindOneProduct = service.getOneProduct(id);
+
+        assertThat(serviceFindOneProduct).isEqualTo(productDTO);
+    }
+
+    @Test
+    void productService_getOneProduct_throwsRecordNotFoundExt() {
+        var id = UUID.randomUUID();
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> service.getOneProduct(id))
+                .isInstanceOf(RecordNotFoundExt.class)
+                .hasMessageContaining("Product with id " + id);
+    }
+
+    // deletion method already tested in repository tests
 }
