938e71b0d27182baee3ee23efc2785afeaea92b2
Luiz Vinicius <viniciusadr244@gmail.com>
2025-02-06 17:31:25 -0300

    feat: manter JSON formatado

--- src/main/java/luiz/api/products/exceptions/RecordNotFoundExt.java
+++ src/main/java/luiz/api/products/exceptions/RecordNotFoundExt.java
@@ -2,6 +2,6 @@ package luiz.api.products.exceptions;
 
 public class RecordNotFoundExt extends RuntimeException {
     public RecordNotFoundExt(String entity) {
-        super(String.format("{error: %s NOT FOUND}", entity));
+        super(String.format("{error: \"%s NOT FOUND\"}", entity));
     }
 }
